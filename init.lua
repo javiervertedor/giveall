2cc09f4690bdcab09441960ec372f08d5c61973b
javiervertedor <78948424+javiervertedor@users.noreply.github.com>
2025-05-31 10:39:45 +0800

    Limit max number of items to 99

--- init.lua
+++ init.lua
@@ -1,10 +1,12 @@
 minetest.register_chatcommand("giveall", { 
     params = "<itemstring> [count]",
-    description = "Give an item to all online players",
+    description = "Give an item to all online players (max 99)",
     privs = {give = true},
     func = function(name, param)
         local item, count = param:match("^(%S+)%s*(%d*)$")
         count = tonumber(count) or 1
+        -- Limit count to 99
+        count = math.min(count, 99)
 
         if not item or item == "" then
             return false, "Usage: /giveall <itemstring> [count]"
